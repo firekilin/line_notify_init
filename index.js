f6994c360a0c858d2792276352c851783da40226
firekilin <wl00161839@gmail.com>
2022-07-13 15:50:03 +0800

    test

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express=require('express');
+const app=express();
+const fetch=require('node-fetch');
+
+//使用者訂閱
+//https: //notify-bot.line.me/oauth/authorize?response_type=code&client_id=ngegPHE6scsOCHLoJ32gsp&redirect_uri=https://icecube.service.com/linenotify&scope=notify&state=NO_STATE
+//
+//我的個人網站：https://icecube.service.com
+//我將使用nginx 把這app轉至：https://icecube.service.com/linenotify
+
+app.listen("5270",()=>{
+  console.log("listen 5270");
+});
+
+//建置callback
+app.get("/",async(req,res)=>{
+  console.log(req.body.code);
+  registerToken(req.body.code);
+  res.send("OK");
+});
+
+//註冊 token
+let registerToken=(code)=>{
+  let url="https://notify-bot.line.me/oauth/token";
+  const ans=fetch(url,{
+    Method:"POST",
+    "Content-Type":"application/x-www-form-urlencoded",
+    body:{
+      grant_type:"authorization_code",
+      code:code,
+      redirect_uri:"https://icecube.service.com/linenotify",
+      client_id:process.env.CLIENT_ID,
+      client_secret:process.env.CLIENT_SECRET
+    }
+  });
+  console.log(ans);
+}
