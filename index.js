d5103a54223556c9117d8bd25d4c7d1152e88dc7
firekilin <wl00161839@gmail.com>
2022-07-13 22:24:01 +0800

    test

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/",express.json(),async(req,res)=>{
   console.log(req.query.code);
   let token =await registerToken(req.query.code);
   let response=token.json();
-  console.log(response);
+  console.log(JSON.stringify(response));
   await lineNotifyMessage(JSON.stringify(response.access_token),"test");
   res.send("OK");
 });
